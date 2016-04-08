b4640ffd27af0581e3d06c0005159a925c8b44f5
Chad Duffey <chadduffey@hotmail.com>
2016-04-08 15:29:48 -0700

    folder listing working

--- RenameFolder/Form1.Designer.cs
+++ RenameFolder/Form1.Designer.cs
@@ -28,20 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.txtFileAccessToken = new System.Windows.Forms.TextBox();
+            this.txtToken = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.btnConnect = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pictureBoxStatus = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboUserList = new System.Windows.Forms.ComboBox();
+            this.btnListMemberFolders = new System.Windows.Forms.Button();
+            this.txtFolderList = new System.Windows.Forms.Label();
+            this.comboFolderList = new System.Windows.Forms.ComboBox();
+            this.txtTargetAccount = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStatus)).BeginInit();
             this.SuspendLayout();
             // 
-            // txtFileAccessToken
+            // txtToken
             // 
-            this.txtFileAccessToken.Location = new System.Drawing.Point(106, 12);
-            this.txtFileAccessToken.Name = "txtFileAccessToken";
-            this.txtFileAccessToken.Size = new System.Drawing.Size(426, 20);
-            this.txtFileAccessToken.TabIndex = 0;
+            this.txtToken.Location = new System.Drawing.Point(106, 12);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.PasswordChar = '*';
+            this.txtToken.Size = new System.Drawing.Size(382, 20);
+            this.txtToken.TabIndex = 0;
             // 
             // label1
             // 
@@ -55,6 +64,7 @@
             // 
             // btnConnect
             // 
+            this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnConnect.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnConnect.Location = new System.Drawing.Point(106, 38);
             this.btnConnect.Name = "btnConnect";
@@ -62,29 +72,102 @@
             this.btnConnect.TabIndex = 2;
             this.btnConnect.Text = "Connect to Dropbox";
             this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
             // 
-            // pictureBox1
+            // pictureBoxStatus
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(495, 39);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(37, 22);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
+            this.pictureBoxStatus.Location = new System.Drawing.Point(494, 12);
+            this.pictureBoxStatus.Name = "pictureBoxStatus";
+            this.pictureBoxStatus.Size = new System.Drawing.Size(37, 22);
+            this.pictureBoxStatus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxStatus.TabIndex = 3;
+            this.pictureBoxStatus.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Network-Connected-icon.png");
+            this.imageList1.Images.SetKeyName(1, "Network-Disconnected-icon.png");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(5, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Target Account";
+            // 
+            // comboUserList
+            // 
+            this.comboUserList.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboUserList.FormattingEnabled = true;
+            this.comboUserList.Location = new System.Drawing.Point(106, 69);
+            this.comboUserList.Name = "comboUserList";
+            this.comboUserList.Size = new System.Drawing.Size(382, 21);
+            this.comboUserList.TabIndex = 5;
+            this.comboUserList.SelectedIndexChanged += new System.EventHandler(this.comboUserList_SelectedIndexChanged);
+            // 
+            // btnListMemberFolders
+            // 
+            this.btnListMemberFolders.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnListMemberFolders.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListMemberFolders.Location = new System.Drawing.Point(106, 97);
+            this.btnListMemberFolders.Name = "btnListMemberFolders";
+            this.btnListMemberFolders.Size = new System.Drawing.Size(382, 23);
+            this.btnListMemberFolders.TabIndex = 6;
+            this.btnListMemberFolders.Text = "List Member Folders";
+            this.btnListMemberFolders.UseVisualStyleBackColor = true;
+            this.btnListMemberFolders.Click += new System.EventHandler(this.btnListMemberFolders_Click);
+            // 
+            // txtFolderList
+            // 
+            this.txtFolderList.AutoSize = true;
+            this.txtFolderList.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFolderList.Location = new System.Drawing.Point(5, 130);
+            this.txtFolderList.Name = "txtFolderList";
+            this.txtFolderList.Size = new System.Drawing.Size(98, 13);
+            this.txtFolderList.TabIndex = 7;
+            this.txtFolderList.Text = "Folder to Rename";
+            // 
+            // comboFolderList
+            // 
+            this.comboFolderList.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboFolderList.FormattingEnabled = true;
+            this.comboFolderList.Location = new System.Drawing.Point(106, 126);
+            this.comboFolderList.Name = "comboFolderList";
+            this.comboFolderList.Size = new System.Drawing.Size(382, 21);
+            this.comboFolderList.TabIndex = 8;
+            // 
+            // txtTargetAccount
+            // 
+            this.txtTargetAccount.Location = new System.Drawing.Point(106, 69);
+            this.txtTargetAccount.Name = "txtTargetAccount";
+            this.txtTargetAccount.Size = new System.Drawing.Size(382, 20);
+            this.txtTargetAccount.TabIndex = 9;
+            this.txtTargetAccount.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(552, 261);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtTargetAccount);
+            this.Controls.Add(this.comboFolderList);
+            this.Controls.Add(this.txtFolderList);
+            this.Controls.Add(this.btnListMemberFolders);
+            this.Controls.Add(this.comboUserList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBoxStatus);
             this.Controls.Add(this.btnConnect);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtFileAccessToken);
+            this.Controls.Add(this.txtToken);
             this.Name = "Form1";
-            this.Text = "Rename Folder";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Text = "Rename Dropbox Folder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStatus)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -92,10 +175,17 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txtFileAccessToken;
+        private System.Windows.Forms.TextBox txtToken;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnConnect;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxStatus;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ComboBox comboUserList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnListMemberFolders;
+        private System.Windows.Forms.Label txtFolderList;
+        private System.Windows.Forms.ComboBox comboFolderList;
+        private System.Windows.Forms.TextBox txtTargetAccount;
     }
 }
 
--- RenameFolder/Form1.cs
+++ RenameFolder/Form1.cs
@@ -7,14 +7,447 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.Serialization;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using Dropbox.Api;
 
 namespace RenameFolder
 {
     public partial class Form1 : Form
     {
+        DfBTeam gbl_TeamObject;
+
+        [DataContract]
+        class FolderListing
+        {
+            //folder listing:
+            [DataMember]
+            public string size { get; set; }
+            [DataMember]
+            public string hash { get; set; }
+            [DataMember]
+            public double bytes { get; set; }
+            [DataMember]
+            public string thumb_exists { get; set; }
+            [DataMember]
+            public string rev { get; set; }
+            [DataMember]
+            public string modified { get; set; }
+            [DataMember]
+            public string path { get; set; }
+            [DataMember]
+            public string is_dir { get; set; }
+            [DataMember]
+            public string icon { get; set; }
+            [DataMember]
+            public string root { get; set; }
+            [DataMember]
+            public string revision { get; set; }
+
+            //not in sample
+            [DataMember]
+            public string is_deleted { get; set; }
+            [DataMember]
+            public string photo_info { get; set; }
+            [DataMember]
+            public string video_info { get; set; }
+            [DataMember]
+            public string client_mtime { get; set; }
+            [DataMember]
+            public List<Shared_Folder> shared_folder { get; set; }
+            [DataMember]
+            public string read_only { get; set; }
+            [DataMember]
+            public string parent_shared_folder_id { get; set; }
+            [DataMember]
+            public string modifier { get; set; }
+            [DataMember]
+            public List<FolderContent> contents { get; set; }
+        }
+
+        [DataContract]
+        class Shared_Folder
+        {
+            //permissions:
+            [DataMember]
+            public string shared_folder_id { get; set; }
+        }
+
+        [DataContract]
+        class FolderContent
+        {
+            [DataMember]
+            public string size { get; set; }
+            [DataMember]
+            public string rev { get; set; }
+            [DataMember]
+            public double bytes { get; set; }
+            [DataMember]
+            public string thumb_exists { get; set; }
+            [DataMember]
+            public string modified { get; set; }
+            [DataMember]
+            public string client_mtime { get; set; }
+            [DataMember]
+            public string path { get; set; }
+            [DataMember]
+            public string photo_info { get; set; }
+            [DataMember]
+            public string is_dir { get; set; }
+            [DataMember]
+            public string icon { get; set; }
+            [DataMember]
+            public string root { get; set; }
+            [DataMember]
+            public string mime_type { get; set; }
+            [DataMember]
+            public string revision { get; set; }
+        }
+        [DataContract]
+        class BasicTeamInformation
+        {
+            //folder listing:
+            [DataMember]
+            public string name { get; set; }
+            [DataMember]
+            public string team_id { get; set; }
+            [DataMember]
+            public int num_licenced_users { get; set; }
+            [DataMember]
+            public int num_provisioned_users { get; set; }
+        }
+
+        [DataContract]
+        class Profile
+        {
+            //profile:
+            [DataMember]
+            public string given_name { get; set; }
+            [DataMember]
+            public string surname { get; set; }
+            [DataMember]
+            public string status { get; set; }
+            [DataMember]
+            public string member_id { get; set; }
+            [DataMember]
+            public string email { get; set; }
+            [DataMember]
+            public string external_id { get; set; }
+            [DataMember]
+            public List<string> groups { get; set; }
+        }
+
+        [DataContract]
+        class Permissions
+        {
+            //permissions:
+            [DataMember]
+            public bool is_admin { get; set; }
+        }
+
+        [DataContract]
+        class Member
+        //member is made up of profile and permissions components
+        {
+            [DataMember]
+            public Profile profile { get; set; }
+            [DataMember]
+            public Permissions permissions { get; set; }
+        }
+
+        [DataContract]
+        class DfBTeam
+        {
+            //team is made up of members and has a cursor and has_more attribute
+            [DataMember]
+            public List<Member> members { get; set; }
+            [DataMember]
+            public string cursor { get; set; }
+            [DataMember]
+            public bool has_more { get; set; }
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //set the disconnected picture
+            pictureBoxStatus.Image = imageList1.Images[1];
+
+            //hide the stuff we dont need yet
+            label2.Visible = false;
+            comboUserList.Visible = false;
+            btnListMemberFolders.Visible = false;
+            comboFolderList.Visible = false;
+            txtFolderList.Visible = false;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            // Set cursor as hourglass
+            Cursor.Current = Cursors.WaitCursor;
+
+            //obtain basic api information
+            BasicTeamInformation teamInformation = dfbBasicTeamInfo();
+            //obtain team information
+            DfBTeam dfbTeam = dfb_members_list();
+
+            //if we connect to the api successfully:
+            if (teamInformation.num_licenced_users >= 0)
+            {
+                //update the connected Image
+                pictureBoxStatus.Image = imageList1.Images[0];
+
+                //populate the global object for use throughout the app.
+                gbl_TeamObject = dfbTeam;
+
+                //disable the button and the input box
+                txtToken.ReadOnly = true;
+                btnConnect.Visible = false;
+
+                //reveal the user list
+                label2.Visible = true;
+                comboUserList.Visible = true;
+                btnListMemberFolders.Visible = true;
+
+                //populate the combo box
+                foreach (Member member in gbl_TeamObject.members)
+                    comboUserList.Items.Add(member.profile.email);
+            }
+
+            // Set cursor as default arrow
+            Cursor.Current = Cursors.Default;
+        }
+
+        private BasicTeamInformation dfbBasicTeamInfo()
+        {
+            String page = "https://api.dropbox.com/1/team/get_info";
+            WebRequest request = WebRequest.Create(page);
+
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            String authheader = "Authorization:Bearer " + txtToken.Text;
+            request.Headers.Add(authheader);
+
+            // Create POST data and convert it to a byte array.
+            string postData = "{}";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            // Set the ContentLength property of the WebRequest.
+            request.ContentLength = byteArray.Length;
+
+            // Get the request stream.
+            Stream ds = request.GetRequestStream();
+
+            // Write the data to the request stream.
+            ds.Write(byteArray, 0, byteArray.Length);
+
+            // Close the Stream object.
+            ds.Close();
+
+            // Get the response.
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                // Get the stream containing content returned by the server.
+                Stream dataStream = response.GetResponseStream();
+
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+
+                // Read the content. 
+                string responseFromServer = reader.ReadToEnd();
+
+                // Cleanup the streams and the response.
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+
+                //serialise the json so we can send back a BasicTeamInformation object
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(BasicTeamInformation));
+                MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(responseFromServer));
+                BasicTeamInformation teamObj = (BasicTeamInformation)ser.ReadObject(stream);
+
+                return teamObj;
+            }
+
+            catch (WebException error)
+            {
+                BasicTeamInformation failedObj = new BasicTeamInformation();
+                failedObj.name = error.Message;
+                failedObj.num_licenced_users = -1;
+                failedObj.num_provisioned_users = -1;
+                return failedObj;
+            }
+        }
+
+        private DfBTeam dfb_members_list()
+        {
+            String page = "https://api.dropbox.com/1/team/members/list";
+            WebRequest request = WebRequest.Create(page);
+
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            String authheader = "Authorization:Bearer " + txtToken.Text;
+            request.Headers.Add(authheader);
+
+            // Create POST data and convert it to a byte array.
+            string postData = "{}";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            // Set the ContentLength property of the WebRequest.
+            request.ContentLength = byteArray.Length;
+
+            // Get the request stream.
+            Stream ds = request.GetRequestStream();
+
+            // Write the data to the request stream.
+            ds.Write(byteArray, 0, byteArray.Length);
+
+            // Close the Stream object.
+            ds.Close();
+
+            // Get the response.
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                // Get the stream containing content returned by the server.
+                Stream dataStream = response.GetResponseStream();
+
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+
+                // Read the content. 
+                string responseFromServer = reader.ReadToEnd();
+
+                // Cleanup the streams and the response.
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+
+                //serialise the json so we can send back a BasicTeamInformation object
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(DfBTeam));
+                MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(responseFromServer));
+                DfBTeam teamObj = (DfBTeam)ser.ReadObject(stream);
+
+                return teamObj;
+            }
+
+            catch (WebException error)
+            {
+                DfBTeam failedObj = new DfBTeam();
+                failedObj.cursor = error.Message;
+                return failedObj;
+            }
+
+        }
+
+        private void btnListMemberFolders_Click(object sender, EventArgs e)
+        {
+
+            // Set cursor as hourglass
+            Cursor.Current = Cursors.WaitCursor;
+
+            FolderListing rootFolderListing = Get_listing("/", txtToken.Text, gbl_TeamObject.members[5].profile.member_id);
+            foreach (FolderContent fc in rootFolderListing.contents)
+            {
+                //if (fc.is_dir)
+                comboFolderList.Items.Add(fc.path);
+            }
+
+            //update the ui
+            comboFolderList.Visible = true;
+            txtFolderList.Visible = true;
+            btnListMemberFolders.Visible = false;
+            comboUserList.Visible = false;
+            txtTargetAccount.Visible = true;
+            txtTargetAccount.ReadOnly = true;
+
+            
+
+            // Set cursor as hourglass
+            Cursor.Current = Cursors.Default;
+
+        }
+
+        private void comboUserList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtTargetAccount.Text = comboUserList.Text;
+        }
+
+        private FolderListing Get_listing(string folderpath, string dfb_token, string dfb_member_id)
+        {
+
+            if (folderpath == null)
+            {
+                Console.WriteLine("Empty path");
+            }
+
+            else
+            {
+                string uri = "https://api.dropbox.com/1/metadata/auto" + folderpath; // +"?list=false";
+                WebRequest request = WebRequest.Create(uri);
+
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                String authheader = "Authorization:Bearer " + dfb_token;
+                request.Headers.Add(authheader);
+                request.Headers.Add("X-Dropbox-Perform-As-Team-Member:" + dfb_member_id);
+
+                try
+                {
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                    // Get the stream containing content returned by the server.
+                    Stream dataStream = response.GetResponseStream();
+
+                    // Open the stream using a StreamReader for easy access.
+                    StreamReader reader = new StreamReader(dataStream);
+
+                    // Read the content. 
+                    string responseFromServer = reader.ReadToEnd();
+
+                    //serialize the json
+                    DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(FolderListing));
+                    MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(responseFromServer));
+                    try
+                    {
+                        var obj = (FolderListing)ser.ReadObject(stream);
+
+                        // Cleanup the streams and the response.
+                        reader.Close();
+                        dataStream.Close();
+                        response.Close();
+
+                        return obj;
+                    }
+                    catch
+                    {
+                        FolderListing failListing = new FolderListing();
+                        failListing.bytes = -2;
+                        return failListing;
+                    }
+
+                }
+
+                catch (WebException error)
+                {
+                    Console.WriteLine(error);
+                }
+
+            }
+            FolderListing brokenListing = new FolderListing();
+            brokenListing.bytes = -1;
+            return brokenListing;
+        } //end of get_listing
+
+
     }
 }
